a1599e059b5e04a4b47214f9844f2ff88ba9105c
Ivan <1>
2023-07-20 21:41:13 +0500

    final version

--- 3.7.5. corrected.py
+++ 3.7.5. corrected.py
@@ -4,26 +4,26 @@
 
 def read_file(file_in_name):
     with open(file_in_name, 'r') as file_in:
-        student_count = {}  # счётсчик учеников каждого класса класса
-        class_list = {}  # счётчик суммарного роста учеников каждого класса
+        student_count = {}
+        class_total_height = {}
         for line in file_in:
             student = [str(i) for i in line.split()]
             student_class = int(student[0])
             student_height = int(student[2])
-            if student_class in class_list:
-                class_list[student_class] += student_height
+            if student_class in class_total_height:
+                class_total_height[student_class] += student_height
                 student_count[student_class] += 1
-            if student_class not in class_list:
-                class_list[student_class] = student_height
+            if student_class not in class_total_height:
+                class_total_height[student_class] = student_height
                 student_count[student_class] = 1
-        return student_count, class_list
+        return student_count, class_total_height
 
 
-def list_of_students(student_count, class_list):
+def list_of_students(student_count, class_total_height):
     class_list_output = {1: '-', 2: '-', 3: '-', 4: '-', 5: '-', 6: '-', 7: '-', 8: '-', 9: '-', 10: '-',
                          11: '-'}
-    for key in class_list:  # изменение значений в конечном словаре
-        class_list_output[key] = class_list[key] / student_count[key]
+    for key in class_total_height:  # изменение значений в конечном словаре
+        class_list_output[key] = class_total_height[key] / student_count[key]
     sorted_list = sorted(class_list_output.items())
     return sorted_list
 
@@ -41,12 +41,11 @@ def main():
     student_count, class_list = read_file('dataset_1.txt')
     final_result = list_of_students(student_count, class_list)
     write_file('output.txt', final_result)
-    # функция берёт все написанные функции и выдаёт на выходе результат программы
+
 
 
 if __name__ == '__main__':
     main()
 
-#закомитить изменения, выделить ещё онду функцию которая делает вычисления (выделить из ф-ции write)
-#подумать над более понятными именами переменных
-# создать текстовый/вордовский док (или тетрадь) для заметок
+
+
