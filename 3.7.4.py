aef4d0e9c309ad9636d30a1a3ca99d004bd44a91
Ivan <1>
2023-07-23 00:18:56 +0500

    function added

--- 3.7.4.py
+++ 3.7.4.py
@@ -1,28 +1,29 @@
-def main():
-    pass
 
+
+
+
+
+
+def movement(directions_count):
+    latitude = 0
+    longitude = 0
+    for i in range(directions_count):
+        direction = input().split(' ')
+        direction[1] = int(direction[1])
+        if direction[0] == 'восток':
+            latitude += direction[1]
+        if direction[0] == 'запад':
+            latitude -= direction[1]
+        if direction[0] == 'север':
+            longitude += direction[1]
+        if direction[0] == 'юг':
+            longitude -= direction[1]
+    return latitude, longitude
+
+
+def main():
+    latitude_end, longitude_end = movement(int(input()))
+    print(latitude_end, longitude_end)
 
 if __name__ == '__main__':
     main()
-    directions_count = int(input())
-
-    def movement():
-        latitude = 0
-        longitude = 0
-        direction_list = []
-        for i in range(directions_count):
-            direction = input().split(' ')
-            direction[1] = int(direction[1])
-            if direction[0] == 'восток':
-                latitude += direction[1]
-            if direction[0] == 'запад':
-                latitude -= direction[1]
-            if direction[0] == 'север':
-                longitude += direction[1]
-            if direction[0] == 'юг':
-                longitude -= direction[1]
-        print(latitude,longitude)
-
-movement()
-
-
