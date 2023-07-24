c505c935bc95dbfe7e9f4bcd18655312ad2c9485
Ivan <1>
2023-07-24 20:44:57 +0500

    function added

--- Zadachi 3.7.1.py
+++ Zadachi 3.7.1.py
@@ -1,29 +1,42 @@
-gamestotal = int(input())
-teamsresoults = {}
-for i in range(gamestotal):                                 #считваем колво игр и результаты команд
-    team1,score1,team2,score2 = input().split(';')          #вводим команды и их результат
-    if team1 not in teamsresoults:                          #проверяем наличие команды 1 в словаре
-        teamsresoults[team1] = [1, 0, 0, 0, 0]
-    elif team1 in teamsresoults:                            #если уже есть в словаре, прибавляем кол-во игр
-        teamsresoults[team1][0] += 1
-    if team2 not in teamsresoults:                          #проверяем наличие команды 2 в словаре
-        teamsresoults[team2] = [1, 0, 0, 0, 0]
-    elif team2 in teamsresoults:
-        teamsresoults[team2][0] += 1                        #прибавляем кол-во игр
-    score1 = int(score1)                                    #преобразуем счёт в числа
-    score2 = int(score2)
-    if score1 > score2:                                     #если победа команды1
-        teamsresoults[team1][1] += 1                        # +1 победа
-        teamsresoults[team1][4] += 3                        # +3 очка за победу
-        teamsresoults[team2][3] += 1                        # +1 поражение команды2
-    if score1 == score2:
-        teamsresoults[team1][2] += 1                        # +1 ничья команды1
-        teamsresoults[team2][2] += 1                        # +1 ничья команды2
-        teamsresoults[team1][4] += 1                        # +1 очко
-        teamsresoults[team2][4] += 1                        # +1 очко
-    if score1 < score2:
-        teamsresoults[team1][3] += 1                        # +1 поражение команды1
-        teamsresoults[team2][1] += 1                        # +1 победа команды2
-        teamsresoults[team2][4] += 3                        # +3 очка команды2
-for key, value in teamsresoults.items():                    # выводим результат
-    print((key+':'), *value, end='\n')
+def results(gamestotal):
+    teamsresults = {}
+    for i in range(gamestotal):                                 #считваем колво игр и результаты команд
+        team1,score1,team2,score2 = input().split(';')          #вводим команды и их результат
+        if team1 not in teamsresults:                          #проверяем наличие команды 1 в словаре
+            teamsresults[team1] = [1, 0, 0, 0, 0]
+        elif team1 in teamsresults:                            #если уже есть в словаре, прибавляем кол-во игр
+            teamsresults[team1][0] += 1
+        if team2 not in teamsresults:                          #проверяем наличие команды 2 в словаре
+            teamsresults[team2] = [1, 0, 0, 0, 0]
+        elif team2 in teamsresults:
+            teamsresults[team2][0] += 1                        #прибавляем кол-во игр
+        score1 = int(score1)                                    #преобразуем счёт в числа
+        score2 = int(score2)
+        if score1 > score2:                                     #если победа команды1
+            teamsresults[team1][1] += 1                        # +1 победа
+            teamsresults[team1][4] += 3                        # +3 очка за победу
+            teamsresults[team2][3] += 1                        # +1 поражение команды2
+        if score1 == score2:
+            teamsresults[team1][2] += 1                        # +1 ничья команды1
+            teamsresults[team2][2] += 1                        # +1 ничья команды2
+            teamsresults[team1][4] += 1                        # +1 очко
+            teamsresults[team2][4] += 1                        # +1 очко
+        if score1 < score2:
+            teamsresults[team1][3] += 1                        # +1 поражение команды1
+            teamsresults[team2][1] += 1                        # +1 победа команды2
+            teamsresults[team2][4] += 3                        # +3 очка команды2
+    return teamsresults
+
+def print_result(teamsresults):
+    for key, value in teamsresults.items():                    # выводим результат
+        print((key+':'), *value, end='\n')
+
+
+def main():
+    gamestotal = int(input())
+    teamresults = results(gamestotal)
+    print_result(teamresults)
+
+
+if __name__ == '__main__':
+    main()
--- Zadachi 3.7.2.py
+++ Zadachi 3.7.2.py
@@ -1,31 +1,49 @@
-ishodn = str(input())
-konechn = str(input())
-shifrov = str(input())
-rashifr = str(input())
-a = list(ishodn)
-b = list(konechn)
-c = list(shifrov)
-d = list(rashifr)
-kod = {}
-kod1 = {}
-x = 0
-n = 0
-l = 0
-n1 = 0
-for char in range(len(ishodn)):
-    kod[ishodn[x]] = konechn[x]
-    x += 1
-for char1 in range(len(konechn)):
-    kod1[konechn[l]] = ishodn[l]
-    l += 1
-for i in shifrov:
-    if i in kod:
-        c[n] = kod[i]
-        n += 1
-for j in rashifr:
-    if j in kod1:
-        d[n1] = kod1[j]
-        n1 += 1
-print(''.join(c))
-print(''.join(d))
+def encrypt(original_text, final_text):
+    original_text = list(original_text)
+    final_text = list(final_text)
+    kod = {}
+    kod1 = {}
+    x = 0
+    l = 0
+    for char in range(len(original_text)):
+        kod[original_text[x]] = final_text[x]
+        x += 1
+    for char1 in range(len(final_text)):
+        kod1[final_text[l]] = original_text[l]
+        l += 1
+    return kod, kod1
 
+
+def decrypt(encrypted_text, decrypted_text, kod, kod1):
+    encrypted_text = list(encrypted_text)
+    decrypted_text = list(decrypted_text)
+    n = 0
+    n1 = 0
+    for i in encrypted_text:
+        if i in kod:
+            encrypted_text[n] = kod[i]
+            n += 1
+    for j in decrypted_text:
+        if j in kod1:
+            decrypted_text[n1] = kod1[j]
+            n1 += 1
+    return encrypted_text, decrypted_text
+
+
+def print_result(encrypted_text, decrypted_text):
+    print(''.join(encrypted_text))
+    print(''.join(decrypted_text))
+
+
+def main():
+    original_text = str(input())
+    final_text = str(input())
+    kod, kod1 = encrypt(original_text, final_text)
+    encrypted_text = str(input())
+    decrypted_text = str(input())
+    encrypted_text1, decrypted_text1 = decrypt(encrypted_text, decrypted_text, kod, kod1)
+    print_result(encrypted_text1,decrypted_text1)
+
+
+if __name__ == '__main__':
+    main()
